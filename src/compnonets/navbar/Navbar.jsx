b718f0c488acd8f29f39e508e052470c5015941d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-16 21:53:47 +0100

    new

--- src/compnonets/navbar/Navbar.jsx
+++ src/compnonets/navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import './Navbar.scss'
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 export default function Navbar() {
   return (
     <div className='navbar'>
     <div className="wrapper">
     <div className="left">
-     <a href=''> jkdkdkkl</a> </div>
+     <a href='#intro' className='logo'> cynthia..</a> 
+     <div className='item-container'>
+     <PersonIcon className='icon'/>
+     <span>+234 8097 407 765</span>
+     </div>
+     <div className='item-container'>
+     <MailIcon className='icon'/>
+     <span>cynthiachiagu@gmail.com</span>
+
+     
+     </div>
+     
+     </div>
     <div className="right"></div>
     
     </div>
