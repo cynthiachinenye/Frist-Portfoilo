6170a90de00f1029dd8122d39b4981c23707e9d7
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-18 16:37:38 +0100

    new

---
+++ src/data.js
@@ -0,0 +1,41 @@
+
+
+export const  portfolioData = [
+    {
+        id: 1,
+        title:'Social Media App',
+        img:'https://www.istockphoto.com/vector/hand-holding-smartphone-with-colorful-social-media-icons-gm1484868109-510988037',
+    },
+
+    {
+        id: 2,
+        title:'Social Media App',
+        img:  'https://www.istockphoto.com/vector/business-reporting-dashboard-concept-illustration-gm1428470293-471951872',
+    },
+
+    {
+        id: 3,
+        title:'Social Media App',
+        img:  'https://www.istockphoto.com/vector/business-reporting-dashboard-concept-illustration-gm1428470293-471951872',
+    },
+
+    {
+        id: 4,
+        title:'Social Media App',
+        img:  'https://www.istockphoto.com/vector/business-reporting-dashboard-concept-illustration-gm1428470293-471951872',
+    },
+
+    {
+        id: 5,
+        title:'Social Media App',
+        img:  'https://www.istockphoto.com/vector/business-reporting-dashboard-concept-illustration-gm1428470293-471951872',
+    },
+
+    {
+        id: 6,
+        title:'Social Media App',
+        img:  'Assets/set.png',
+    },
+
+
+]
