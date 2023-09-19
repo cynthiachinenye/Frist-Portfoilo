7a031d76c37479a242e52f01025eda55013d9027
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-19 13:17:18 +0100

    new

--- src/compnonets/works/Works.jsx
+++ src/compnonets/works/Works.jsx
@@ -3,21 +3,29 @@ import './work.scss'
 export default function Works() {
   return (
     <div className='works' id='works'>
-    <div className='slider'>
-      <div className='container'>
-       <div className='item'>
-       <div className='left'>
-        <div className='leftContainer'>
-        <div className='imgContainer'>
-        <img src='Assest/phone.png'/>
+      <div className='slider'>
+        <div className='container'>
+          <div className='item'>
+            <div className='left'>
+              <div className='leftContainer'>
+                <div className='imgContainer'>
+                  <img src='Assest/phone.png' />
+                  <h2>Title</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc vitae mi diam. Vestibulum ante ipsum primis in faucibus
+                    orci luctus et ultrices posuere cubilia curae;
+                    Sed consectetur eu nibh in fermentum.
+                    Mauris luctus pulvinar leo malesuada tristique.
+                    Proin sed varius erat.
+                    Cras condimentum mauris id augue congue fringilla.</p>
+                </div>
+              </div>
+            </div>
+            <div className='right'></div>
+          </div>
         </div>
-        </div>
-       </div>
-       <div className='right'></div>
-       </div>
       </div>
-    </div>
-      
+
     </div>
   )
 }
