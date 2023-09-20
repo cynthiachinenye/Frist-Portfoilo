26a1721d3f1f8d4b793303a88dd39d033d5a06ab
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-20 04:17:30 +0100

    new

--- src/compnonets/testimonials/Testimonial.jsx
+++ src/compnonets/testimonials/Testimonial.jsx
@@ -3,6 +3,10 @@ import './testimonial.scss'
 function Testimonial() {
   return (
     <div className="test" id='test'>
+    <h1>Testimonial</h1>
+    <div className='container'>
+     <div className='card'></div>
+    </div>
       
     </div>
   )
