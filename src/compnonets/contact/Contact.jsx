c4b7c4527f2a1380c9cd5ef89ca09af1209836f0
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-20 16:10:23 +0100

    new

--- src/compnonets/contact/Contact.jsx
+++ src/compnonets/contact/Contact.jsx
@@ -3,6 +3,18 @@ import './Contact.scss'
 function Contact() {
   return (
     <div className="contact" id="contact">
+    <div className='left'>
+     <img src='Assets/contact.png' alt=''/>
+    </div>
+    <div className='right'>
+    <h2>Contact</h2>
+       <form>
+        <input type="email" placeholder="Email"/>
+        <textarea placeholder='Message'></textarea>
+        <button type='submit'>Send</button>
+       </form>
+    </div>
+
       
     </div>
   )
