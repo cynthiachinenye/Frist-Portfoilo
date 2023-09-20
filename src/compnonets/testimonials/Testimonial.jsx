50b868b30255c5276de39167948cb8a7e7215b90
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-20 14:43:29 +0100

    new

--- src/compnonets/testimonials/Testimonial.jsx
+++ src/compnonets/testimonials/Testimonial.jsx
@@ -1,26 +1,33 @@
 import './testimonial.scss'
+import { testimonials} from '../../Tests'
 
 function Testimonial() {
   return (
     <div className="test" id='test'>
-    <h1>Testimonial</h1>
-    <div className='container'>
-     <div className='card'>
-       <div className='top'>
-         <img src='Assets/arrow-key.png' className='left' alt=''/>
-         <img  className='user' src='https://images.pexels.com/photos/18125671/pexels-photo-18125671/free-photo-of-sexy-woman-posing-in-dark.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''/>
-         <img className='right' src='Assets/youtube.png' alt=''/>
-       </div>
-       <div className='center'>
-       Lorem ipsum dolor sit amet consectetur adipiscing elit Nunc vitae mi diam Vestibulum ante ipsum primis
-       </div>
-       <div className='bottom'>
-       <h3>Alex</h3>
-       <h4>CEO OF LAMA</h4>
-       </div>
-     </div>
+      <h1>Testimonial</h1>
+      <div className='container'>
+        {
+          testimonials.map((t) => (
+
+            <div className={t.featured ? 'card featured': "card"}>
+              <div className='top'>
+                <img src='Assets/arrow-key.png' className='left' alt='' />
+                <img className='user' src={t.img} alt='' />
+                <img className='right' src={t.icon} alt='' />
+              </div>
+              <div className='center'>
+               {
+                t.desc
+               }</div>
+              <div className='bottom'>
+                <h3>{t.name}</h3>
+                <h4>{t.title}</h4>
+              </div>
+            </div>
+            ))
+          }
     </div>
-      
+
     </div>
   )
 }
