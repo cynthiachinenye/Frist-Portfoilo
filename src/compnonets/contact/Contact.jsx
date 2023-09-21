c203e9f9c20eacd4ad17895f509a0767d1f9285e
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-21 22:25:06 +0100

    new

--- src/compnonets/contact/Contact.jsx
+++ src/compnonets/contact/Contact.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react'
 import './Contact.scss'
 
 function Contact() {
+ const[message,setMessage]=useState(false)
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    setMessage(true)
+  }
   return (
     <div className="contact" id="contact">
     <div className='left'>
@@ -8,10 +14,11 @@ function Contact() {
     </div>
     <div className='right'>
     <h2>Contact</h2>
-       <form>
+       <form onSubmit={submitHandler}>
         <input type="email" placeholder="Email"/>
         <textarea placeholder='Message'></textarea>
         <button type='submit'>Send</button>
+        { message &&  <span>Thanks I'II  reply ASAP :)</span>}
        </form>
     </div>
 
