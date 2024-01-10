2dbc8a7670cf2054a7a0b92ddb960a07fc34f717
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-10 10:56:18 +0100

    new

---
+++ src/compnonets/Config/Config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMX25xD2L3rQjCGUYhXslKjPUuKzSamFI",
+  authDomain: "first-portfolio-efb36.firebaseapp.com",
+  projectId: "first-portfolio-efb36",
+  storageBucket: "first-portfolio-efb36.appspot.com",
+  messagingSenderId: "205579050872",
+  appId: "1:205579050872:web:5b4510fc3d84e307a1b28d",
+  measurementId: "G-PSE6ZHX17N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
